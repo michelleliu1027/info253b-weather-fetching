5a4d19637ed794a072edf2c3846a176ed9a7f6b6
michelleliu1027 <35590106+michelleliu1027@users.noreply.github.com>
2022-04-27 14:34:07 -0700

    update with 3 cities(sf/oakland/bkl)

--- weatherapi-Python-CodeGen-PY/fetching-info253b.py
+++ weatherapi-Python-CodeGen-PY/fetching-info253b.py
@@ -1,20 +1,26 @@
 from weatherapi.weatherapi_client import WeatherapiClient
 import datetime
+import pandas as pd
 
 key = '86fb5d55391f4825963220642222004'
 client = WeatherapiClient(key)
 ap_is_controller = client.ap_is
 
-q = 'Fremont'
-print(q)
-end_dt = datetime.datetime.now().date()
-days = datetime.timedelta(6)
-from_dt = end_dt - days
-print('from date:', from_dt)
-print('end date:', end_dt)
-result = ap_is_controller.get_history_weather(q = q, dt = from_dt, end_dt = end_dt)
-print(len(result.forecast.forecastday))
-print(len(result.forecast.forecastday[0].hour))
-
-for hour_obj in result.forecast.forecastday[0].hour:
-    print(hour_obj.time, hour_obj.feelslike_f)
+cities = ['Berkeley', 'Oakland', 'San Francisco']
+output = {'city_name':[], 'date':[], 'temperature':[]}
+for city in cities:
+    print(city)
+    end_dt = datetime.datetime.now().date()
+    days = datetime.timedelta(6)
+    from_dt = end_dt - days
+    print('from date:', from_dt)
+    print('end date:', end_dt)
+    result = ap_is_controller.get_history_weather(q = city, dt = from_dt, end_dt = end_dt)
+    print(result.forecast.forecastday)
+    for day in result.forecast.forecastday:
+        for hour_obj in day.hour:
+            output['city_name'].append(city)
+            output['date'].append(hour_obj.time)
+            output['temperature'].append(hour_obj.feelslike_f)
+print(pd.DataFrame(output))
+pd.DataFrame(output).to_csv('city_temp.csv')
